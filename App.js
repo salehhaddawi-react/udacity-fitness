9cb62379d3522093afe763819b85a5ac3db23c6f
Saleh Haddawi <saleh.haddawi@gmail.com>
2021-05-29 23:59:29 +0300

    WIP

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app WOOW Saleh!</Text>
+      <Text>I Want to Eat</Text>
+      <Ionicons name="ios-pizza" color="red" size={100}/>
       <StatusBar style="auto" />
     </View>
   );
